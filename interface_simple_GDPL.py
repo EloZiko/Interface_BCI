e36c58460086f92a08e14908accb0bf11c9342c7
EloZiko <ouahidi.zak@gmail.com>
2023-06-11 21:22:11 +0200

    interface finale

--- interface_simple_GDPL.py
+++ interface_simple_GDPL.py
@@ -7,17 +7,21 @@ import argparse
 
 global nombre_mouvements,nombre_repetitions,mouvements,temps_action,temps_pause
 
-mouvements=["Gauche","Droite"]
-nombre_mouvements=2
+mouvements=["Gauche","Droite","Langue","Pieds"]
+nombre_mouvements=4
 nombre_repetitions=30
 temps_action=4
 temps_pause=3
-conditions = list(['_right_','_left_','_feets_'])
+temps_pauselongue=10
+temps_sur_la_croix=2
+conditions = list(['_right_','_left_','_feets_', '_tongue_'])
 
 
 dict_c = {
 '_right_':2,
 '_left_':3,
+'_feets_':4,
+'_tongue_':5
 }
 
 BoardShim.enable_dev_board_logger()
@@ -76,6 +80,7 @@ fenetre_acquisition.config(menu=menu_bar)
 #Gauche
 frame= Frame(fenetre_acquisition,relief=RAISED,borderwidth=1)
 frame.grid(row=1,column=0,padx=5, pady=5)
+
 label_mouvements1=Label(frame,text="Gauche",font=('Arial', 12), width=20, anchor='w')
 label_mouvements1.pack()
 point1 = Label(frame,text=" ", font=('Arial', 80))
@@ -105,10 +110,14 @@ label_mouvements4.pack()
 point4 = Label(frame,text=" ", font=('Arial', 80))
 point4.pack()
 
+
+#Croix
 frame=Frame(fenetre_acquisition)
 frame.grid(row=1,column=1,padx=10, pady=5,sticky="e")
 label_croix=Label(frame,text="                     +",font=('Arial',30),width=20,anchor='w')
 label_croix.pack()
+
+
 
 frame=Frame(fenetre_acquisition,relief=RAISED,borderwidth=1)
 frame.grid(row=3,column=3,padx=5, pady=5,sticky="e")
@@ -117,9 +126,10 @@ label_pause.pack()
 
 
 
-
+#Pause
 pointPause = Label(frame, text=" ", font=('Arial', 80))
 pointPause.pack()
+
 
 labels =[label_mouvements1,label_mouvements2,label_mouvements3,label_mouvements4]
 points=[point1,point2,point3,point4]
@@ -150,18 +160,23 @@ def acquisition():
         for i, (point, label) in enumerate(zip(points, labels)):
             
             if repetition == 10:
-                time.sleep(10)
+                time.sleep(temps_pauselongue)
                 
+            
             winsound.Beep(500, 200)
             label_croix.config(fg="red")
             fenetre_acquisition.update()
-            time.sleep(2)
+            time.sleep(temps_sur_la_croix)
             
 
             label_croix.config(fg="black")
-            point.config(text=".", fg="green")
             label.config(fg="green")
+            point.config(text=".", fg="green")
             winsound.Beep(1000, 200)
+            for (point2,label2) in zip(points,labels):
+                if point2 != point:
+                    point2.config(text=" ")
+                    label2.config(fg="#F0F0F0")
             fenetre_acquisition.update()
             time.sleep(1)
 
@@ -174,16 +189,25 @@ def acquisition():
             label.config(fg="black")
             fenetre_acquisition.update()
 
-            time.sleep(4)
+            time.sleep(temps_action)
 
 
             #Pause
+            
             winsound.Beep(700, 200) 
             pointPause.config(text=".", fg="red")
             label_pause.config(fg="red")
             
+            
+            for (point2,label2) in zip(points,labels):
+                if point2 != point:
+                    point2.config(text=" ")
+                    label2.config(fg="black")
+                
+            
             fenetre_acquisition.update()
-            time.sleep(3)
+
+            time.sleep(temps_pause)    
             pointPause.config(text=" ",)
             label_pause.config(fg="black")
             
